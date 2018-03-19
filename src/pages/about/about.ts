0d29187b2f31d5d77eccae94c6c8e0b0ccfbeb45
Jonas Aschenbrenner <jonas.aschenbrenner@sap.com>
2018-03-19 15:59:55 +0100

    added camer functionallity

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,14 +1,41 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { Camera, CameraOptions } from '@ionic-native/camera';
+import { ToastController } from 'ionic-angular';
+
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
 })
 export class AboutPage {
 
-  constructor(public navCtrl: NavController) {
-
-  }
+  constructor(public navCtrl: NavController, private camera: Camera, public toastCtrl: ToastController) {
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+    
+    this.camera.getPicture(options).then((imageData) => {
+     // imageData is either a base64 encoded string or a file URI
+     // If it's base64:
+     let base64Image = 'data:image/jpeg;base64,' + imageData;
+     let toast = this.toastCtrl.create({
+      message: base64Image,
+      duration: 3000
+    });
+    toast.present();
+    }, (err) => {
+     // Handle error
+     let toast = this.toastCtrl.create({
+      message: err,
+      duration: 5000
+    });
+    toast.present();
+    });
+  
+  }    
 
 }
